d72f93a26051f369861d1350c010ff5a3331df7a
Lander Hernández <hernandez.lander@opendeusto.es>
2022-12-01 17:34:50 +0100

    Se realizan las traducciones al castellano y se limpia un poco mas el codigo

--- Proyecto.py
+++ Proyecto.py
@@ -35,96 +35,91 @@ pink = (255, 0, 255)
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 
-# Initialize videocapture
+# Inicializar videocapture
 cap = cv2.VideoCapture(1)
-# Meta.
+
+# Datos para el videowriter
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_size = (width, height)
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+fourcc = cv2.VideoWriter_fourcc('I', '4', '2', '0')
 
 # Video writer.
-video_output = cv2.VideoWriter('output.mp4', fourcc, fps, frame_size)
+video_output = cv2.VideoWriter('output.avi', fourcc, fps, frame_size)
+
 while(True):
-# Capture frames.
+# Capturar frames.
     success, image = cap.read()
     if not success:
         print("Null.Frames")
     # Get fps.
     fps = cap.get(cv2.CAP_PROP_FPS)
 
-    # Convert the BGR image to RGB.
+    # Convertir la imagen BGR a RGB.
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # Process the image.
+    # Procesar la imagen
     keypoints = pose.process(image)
 
-    # Convert the image back to BGR.
+    # Convertir la imagen de nuevo a BGR.
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    # Body Posture Landmark Coordinates
+    # Coordenadas del Landmark de postura corporal
     lmPose = mp_pose.PoseLandmark
 
-    # Use lm and lmPose as representative of the following methods.
+    # Obtener los landmarks de los keypoints
     lm = keypoints.pose_landmarks
-    if (lm is not None):
+    if lm is not None:
         lmPose = mp_pose.PoseLandmark
-        # Left shoulder.
+        # Hombro izquierdo
         l_shldr_x = int(lm.landmark[lmPose.LEFT_SHOULDER].x * width)
         l_shldr_y = int(lm.landmark[lmPose.LEFT_SHOULDER].y * height)
 
-        # Right shoulder.
+        # Hombro derecho
         r_shldr_x = int(lm.landmark[lmPose.RIGHT_SHOULDER].x * width)
         r_shldr_y = int(lm.landmark[lmPose.RIGHT_SHOULDER].y * height)
 
-        # Left ear.
+        # Oreja izquierda
         l_ear_x = int(lm.landmark[lmPose.LEFT_EAR].x * width)
         l_ear_y = int(lm.landmark[lmPose.LEFT_EAR].y * height)
 
-        # Left hip.
+        # Cadera izquierda
         l_hip_x = int(lm.landmark[lmPose.LEFT_HIP].x * width)
         l_hip_y = int(lm.landmark[lmPose.LEFT_HIP].y * height)
 
-
-    # Calculate distance between left shoulder and right shoulder points.
+        # Calcular la distancia entre los puntos del hombro izquierdo y el hombro derecho
         offset = findDistance(l_shldr_x, l_shldr_y, r_shldr_x, r_shldr_y)
 
-        # Assist to align the camera to point at the side view of the person.
-        # Offset threshold 30 is based on results obtained from analysis over 100 samples.
+        # Alinear la camara al punto de la vista lateral de la persona
+        # El parametro Offset threshold se ha fijado a 30 tras realizar diferentes pruebas
         if offset < 100:
-            cv2.putText(image, str(int(offset)) + ' Aligned', (width - 150, 30), font, 0.9, green, 2)
+            cv2.putText(image, str(int(offset)) + ' Alineado', (width - 175, 30), font, 0.9, green, 2)
         else:
-            cv2.putText(image, str(int(offset)) + ' Not Aligned', (width - 150, 30), font, 0.9, red, 2)
+            cv2.putText(image, str(int(offset)) + ' No alineado', (width - 255, 30), font, 0.9, red, 2)
 
-
-        # Calculate Body Posture Inclination and Draw Landmarks
-        # Calculate angles.
+        # Calcular la inclinacion de la postura corporal y pintar los puntos de referencia
+        # Calcular angulos
         neck_inclination = findAngle(l_shldr_x, l_shldr_y, l_ear_x, l_ear_y)
         torso_inclination = findAngle(l_hip_x, l_hip_y, l_shldr_x, l_shldr_y)
 
-        # Draw landmarks.
+        # Pintar puntos de referencia
         cv2.circle(image, (l_shldr_x, l_shldr_y), 7, yellow, -1)
         cv2.circle(image, (l_ear_x, l_ear_y), 7, yellow, -1)
 
-        # Let's take y - coordinate of P3 100px above x1,  for display elegance.
-        # Although we are taking y = 0 while calculating angle between P1,P2,P3.
+        # Pintamos los puntos de cadera y hombros en la imagen
         cv2.circle(image, (l_shldr_x, l_shldr_y - 100), 7, yellow, -1)
         cv2.circle(image, (r_shldr_x, r_shldr_y), 7, pink, -1)
         cv2.circle(image, (l_hip_x, l_hip_y), 7, yellow, -1)
-
-        # Similarly, here we are taking y - coordinate 100px above x1. Note that
-        # you can take any value for y, not necessarily 100 or 200 pixels.
         cv2.circle(image, (l_hip_x, l_hip_y - 100), 7, yellow, -1)
 
-        # Put text, Posture and angle inclination.
-        # Text string for display.
-        angle_text_string = 'Neck : ' + str(int(neck_inclination)) + '  Torso : ' + str(int(torso_inclination))
+        # Imprimimos el texto por pantalla, inclinacion de postura y angulos
+        angle_text_string = 'Cuello : ' + str(int(neck_inclination)) + '  Torso : ' + str(int(torso_inclination))
 
 
-        # Body Posture Detectino Conditionals
-        # Determine whether good posture or bad posture.
-        # The threshold angles have been set based on intuition.
+        # Condiciones de deteccion de postura corporal
+        # Determinar si la postura es buena o no
+        # Los angulos del threshold han sido fijados tras varias pruebas
         if neck_inclination < 40 and torso_inclination < 10:
             bad_frames = 0
             good_frames += 1
@@ -132,7 +127,7 @@ while(True):
             cv2.putText(image, angle_text_string, (10, 30), font, 0.9, light_green, 2)
             cv2.putText(image, str(int(neck_inclination)), (l_shldr_x + 10, l_shldr_y), font, 0.9, light_green, 2)
             cv2.putText(image, str(int(torso_inclination)), (l_hip_x + 10, l_hip_y), font, 0.9, light_green, 2)
-            # Join landmarks.
+            # Union de puntos de referencia
             cv2.line(image, (l_shldr_x, l_shldr_y), (l_ear_x, l_ear_y), green, 4)
             cv2.line(image, (l_shldr_x, l_shldr_y), (l_shldr_x, l_shldr_y - 100), green, 4)
             cv2.line(image, (l_hip_x, l_hip_y), (l_shldr_x, l_shldr_y), green, 4)
@@ -146,27 +141,29 @@ while(True):
             cv2.putText(image, str(int(neck_inclination)), (l_shldr_x + 10, l_shldr_y), font, 0.9, red, 2)
             cv2.putText(image, str(int(torso_inclination)), (l_hip_x + 10, l_hip_y), font, 0.9, red, 2)
 
-            # Join landmarks.
+            # Union de puntos de referencia
             cv2.line(image, (l_shldr_x, l_shldr_y), (l_ear_x, l_ear_y), red, 4)
             cv2.line(image, (l_shldr_x, l_shldr_y), (l_shldr_x, l_shldr_y - 100), red, 4)
             cv2.line(image, (l_hip_x, l_hip_y), (l_shldr_x, l_shldr_y), red, 4)
             cv2.line(image, (l_hip_x, l_hip_y), (l_hip_x, l_hip_y - 100), red, 4)
 
-        # Calculate the time of remaining in a particular posture.
+        # Calcular el tiempo que el sujeto esta en la postura actual
         good_time = (1 / fps) * good_frames
         bad_time = (1 / fps) * bad_frames
 
-        # Pose time.
+        # Tiempo de pose
         if good_time > 0:
-            time_string_good = 'Good Posture Time : ' + str(round(good_time, 1)) + 's'
+            time_string_good = 'Tiempo de postura correcta : ' + str(round(good_time, 1)) + 's'
             cv2.putText(image, time_string_good, (10, height - 20), font, 0.9, green, 2)
         else:
-            time_string_bad = 'Bad Posture Time : ' + str(round(bad_time, 1)) + 's'
+            time_string_bad = 'Tiempo de postura incorrecta : ' + str(round(bad_time, 1)) + 's'
             cv2.putText(image, time_string_bad, (10, height - 20), font, 0.9, red, 2)
 
     cv2.imshow('frame', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    video_output.write(image)
 
 cap.release()
 cv2.destroyAllWindows()
